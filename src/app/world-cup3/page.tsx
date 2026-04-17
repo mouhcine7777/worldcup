c675aba705f739b77daf6198cc690b6ab2af11f7
unknown <mouchine3@gmail.com>
2026-04-17 16:45:05 +0100

    first commit

--- src/app/world-cup3/page.tsx
+++ src/app/world-cup3/page.tsx
@@ -1,6 +1,4 @@
-import StickyMenu from "./components/StickyMenu";
 import HeroSectionWC from "./components/HeroSectionWC";
-import AboutSection from "./components/AboutSection";
 import PackagesSection from "./components/PackagesSection";
 import SectionStretch from "./components/SectionStretch";
 import HotelSection from "./components/HotelSection";
